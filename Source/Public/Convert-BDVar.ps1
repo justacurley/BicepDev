ce0b504f49f97dda54b03d38e47fae821af89dc2
justacurley <alexmwcurley@gmail.com>
2022-01-22 14:58:27 -0700

    resolve todo, bdvars now live in one output

--- Source/Public/Convert-BDVar.ps1
+++ Source/Public/Convert-BDVar.ps1
@@ -3,17 +3,16 @@ function Convert-BDVar {
     .SYNOPSIS
         Converts all variables in an arm template to ouputs
     .DESCRIPTION
-        This cmdlet adds two outputs to the $BuiltModule arm template: 'bdCopyVars' & 'bdVariables'
+        This cmdlet adds one output to the $BuiltModule arm template: 'bdVars'
 
         When bicep builds to an ARM template, all copy loop array variables are added to a single variable named 'copy'. We use
-        similar logic here to take all of those copy loops inside the copy var, and put them in a new ARM object variable, which
-        we use to make a more simple output in bdCopyVars output. The pattern follows for all other variables, which are outputted
-        in bdVariables.
+        similar logic here to take all of those copy loops inside the copy var, and put them in a new ARM template variable, which
+        we use to make a more simple output in bdVars. The pattern follows for all other variables.
     .EXAMPLE
         PS C:\> New-BDFile -BicepDeploymentFile testAppGateway.bicep -BicepModuleFile appGateway.bicep -outvariable $BD
         PS C:\> $BD.BuiltModule | Convert-BDVar
 
-        Add all variables in $BD.BuiltModule to new variables (bdVariables, bdCopyVars), and output those variables in the arm template.
+        Add all variables in $BD.BuiltModule to new variable (bdVars), and output variable bdVars in the arm template.
     .INPUTS
         (New-BDFile).BuiltModule
     .NOTES
@@ -28,22 +27,20 @@ function Convert-BDVar {
     )
     process {
         $ModuleFileContent = Get-Content $BuiltModule.FullName -Encoding 'utf8' -Raw | ConvertFrom-Json
-        $ModuleFileContent.variables | Add-Member 'bdVariables' ([pscustomobject]@{})
-        $ModuleFileContent.variables | Add-Member 'bdCopyVars' ([pscustomobject]@{})
+        $ModuleFileContent.variables | Add-Member 'bdVars' ([pscustomobject]@{})
         $ModuleFileContent.variables.psobject.Properties | ForEach-Object {
             $name = $_.name
             $value = $_.value
-            if ($name -notin @('copy','bdVariables','bdCopyVars')) {
-                $ModuleFileContent.variables.bdVariables | Add-Member NoteProperty $name $PSItem.Value -Force
+            if ($name -notin @('copy','bdVars')) {
+                $ModuleFileContent.variables.bdVars | Add-Member NoteProperty $name $PSItem.Value -Force
             }
             elseif ($name -eq 'copy') {
                 foreach ($copyArray in $value) {
-                    $ModuleFileContent.variables.bdCopyVars | Add-Member NoteProperty $copyArray.Name ("[variables('{0}')]" -f $copyArray.name) -Force
+                    $ModuleFileContent.variables.bdVars | Add-Member NoteProperty $copyArray.Name ("[variables('{0}')]" -f $copyArray.name) -Force
                 }
             }
         }
-        $ModuleFileContent.outputs | Add-Member NoteProperty "bdCopyVars" @{type = 'object'; value = "[variables('bdCopyVars')]" } -Force
-        $ModuleFileContent.outputs | Add-Member NoteProperty "bdVariables" @{type = 'object'; value = "[variables('bdVariables')]"} -Force
+        $ModuleFileContent.outputs | Add-Member NoteProperty "bdVars" @{type = 'object'; value = "[variables('bdVars')]"} -Force
         $ModuleFileContent | ConvertTo-Json -Depth 99 | Out-File $BuiltModule.FullName
     }
 }
