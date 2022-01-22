caded822c6eabc54a890d0db79cf88df89cd1a71
justacurley <alexmwcurley@gmail.com>
2022-01-22 14:34:20 -0700

    Completed TODOs for remove-bdfile

--- Source/Public/Remove-BDFile.ps1
+++ Source/Public/Remove-BDFile.ps1
@@ -10,26 +10,39 @@ function Remove-BDFile {
     .INPUTS
         (New-BDFile)
     .NOTES
-        TODO search for and remove BDFiles if no parameters are passed in
-        TODO this cmdlet throws when $bd.outputname is piped in
     #>
     [CmdletBinding(SupportsShouldProcess)]
     param (
-        [Parameter(ValueFromPipelineByPropertyName, HelpMessage = "The .bicep file that calls the bicep module we want to transform")]
+        [Parameter(ValueFromPipelineByPropertyName, HelpMessage = "The .bicep file that calls the bicep module we want to transform", ParameterSetName = 'pipe')]
+
         [Alias('Deploy')]
         [System.IO.FileInfo]
         $BicepDeploymentFile,
-        [Parameter(ValueFromPipelineByPropertyName, HelpMessage = "The .bicep module we want to transform")]
+        [Parameter(ValueFromPipelineByPropertyName, HelpMessage = "The .bicep module we want to transform", ParameterSetName = 'pipe')]
         [Alias('Module')]
         [System.IO.FileInfo]
         $BicepModuleFile,
-        [Parameter(ValueFromPipelineByPropertyName, HelpMessage = "The .json file that was built from New-BDFile")]
+        [Parameter(ValueFromPipelineByPropertyName, HelpMessage = "The .json file that was built from New-BDFile", ParameterSetName = 'pipe')]
         [System.IO.FileInfo]
-        $BuiltModule
+        $BuiltModule,
+        [Parameter(HelpMessage = "Search for and remove files matching files created by New-BDFile", ParameterSetName = 'besteffort')]
+        [ValidateScript({Test-Path $_})]
+        [string]
+        $RemoveFromDirectory
     )
     process {
-        $PSBoundParameters.GetEnumerator().ForEach( {
-                $_.Value | Remove-Item -Force
-            })
+        if ($RemoveFromDirectory) {
+            Write-Verbose "Searching for bicep and json files in $RemoveFromDirectory"
+            Get-ChildItem -Path $RemoveFromDirectory -Recurse -File -Include *.bicep,*.json | Where-Object {$_.Name -match "(\d|[a-z]){4}_"} | Foreach-Object {
+                Write-Verbose "Attempting to remove $($_.FullName)"
+                $_ | Remove-Item -Force
+            }
+        }
+        else {
+            $PSBoundParameters.GetEnumerator().ForEach( {
+                    Write-Verbose "Attempting to remove $($_.Value.Name)"
+                    $_.Value | Remove-Item -Force
+                })
+        }
     }
 }
